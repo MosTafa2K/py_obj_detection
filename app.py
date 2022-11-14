b5c147500af595175a1f1e5758900166f35aa9ad
Mostafa <40084943+MosTafa2K@users.noreply.github.com>
2022-11-14 11:35:28 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,33 @@
-from imageai.Detection import ObjectDetection
 import os
+import time
+import cv2
+from PIL import Image
+from imageai.Detection import ObjectDetection, VideoObjectDetection
+
+execution_path = os.getcwd()
+detector = VideoObjectDetection()
+detector.setModelTypeAsYOLOv3()
+
+detector.setModelPath(os.path.join(
+    execution_path, "./Models/yolo.h5"))
+detector.loadModel()
+
+detections = detector.detectObjectsFromVideo(
+    input_file_path=os.path.join(execution_path, "video.mp4"),
+    output_file_path=os.path.join(execution_path, "video-result"),
+    frames_per_second=30,
+    log_progress=True
+)
+
+
+print("-"*50)
+for num, eachObjeect in enumerate(detections, start=1):
+    print(
+        f'{num}) {eachObjeect["name"]}: {eachObjeect["percentage_probability"]}')
+
+
+print("Done with successfully!")
+
+time.sleep(2)
+img = Image.open("result.jpg")
+img.show()
